99400970694e81c3e0a1977a3724e02cb73b61b9
luolin_K <abel.yelin@gmail.com>
2024-11-06 01:05:51 +0800

    Update videos.ts

--- src/config/videos.ts
+++ src/config/videos.ts
@@ -3,10 +3,19 @@ export const featuredVideos = [
     id: 'YK8YdalfwBQ',
     title: 'Puzzle Durov Today',
     description: '探索 Durov 的谜题世界',
+    thumbnailUrl: 'https://img.youtube.com/vi/YK8YdalfwBQ/maxresdefault.jpg',
   },
   {
     id: 'lykPixgrD8Y',
     title: 'Puzzle Durov Today',
     description: '解密 Durov 的挑战',
+    thumbnailUrl: 'https://img.youtube.com/vi/lykPixgrD8Y/maxresdefault.jpg',
   },
 ];
+
+export interface FeaturedVideo {
+  id: string;
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+}
