9d6e6941b234af764b659c40b0a54f7e15fe3761
luolin_K <abel.yelin@gmail.com>
2024-11-06 01:01:05 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,14 @@ import { Metadata } from 'next'
 import { YouTubeEmbed } from '@/components/YouTubeEmbed';
 import { featuredVideos } from '@/config/videos';
 
+interface YouTubeEmbedProps {
+  thumbnailUrl?: string;
+  // ... 其他属性
+}
+
 export const metadata: Metadata = {
   title: 'puzzle durov today',
-  description: 'Here Is Today’s "Major" Telegram Game Puzzle Durov Combo.',
+  description: 'Here Is Todays "Major" Telegram Game Puzzle Durov Combo.',
 }
 
 export default function Home() {
@@ -25,9 +30,9 @@ export default function Home() {
         <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
           puzzle durov today
         </h1>
-        <h2 className="text-2xl tracking-tighter sm:text-3xl md:text-3xl lg:text-3xl">Here Is Today’s "Major" Telegram Game Puzzle Durov Combo.</h2>
+        <h2 className="text-2xl tracking-tighter sm:text-3xl md:text-3xl lg:text-3xl">Here Is Today&apos;s &quot;Major&quot; Telegram Game Puzzle Durov Combo.</h2>
         <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl">
-        Here Is Today’s "Major" Telegram Game Puzzle Durov Combo,MAJOR is now featured in Bitget Pre-Market, giving users the chance to engage in OTC token trades ahead of spot trading. This is an excellent opportunity to enhance your returns!.
+        Here Is Today&apos;s &quot;Major&quot; Telegram Game Puzzle Durov Combo,MAJOR is now featured in Bitget Pre-Market, giving users the chance to engage in OTC token trades ahead of spot trading. This is an excellent opportunity to enhance your returns!.
         </p>
       </section>
       <section className="my-12">
@@ -37,10 +42,10 @@ export default function Home() {
             <YouTubeEmbed
               key={video.id}
               videoId={video.id}
-              thumbnailUrl={`https://img.youtube.com/vi/${video.id}/maxresdefault.jpg`}
               title={video.title}
               description={video.description}
               className="w-full shadow-lg rounded-lg overflow-hidden"
+              thumbnailUrl={video.thumbnailUrl}
             />
           ))}
         </div>
